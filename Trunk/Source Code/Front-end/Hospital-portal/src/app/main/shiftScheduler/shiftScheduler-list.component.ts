c0c9e9c9e3bfeab8095811d8111e7838869b3c21
hoanglong0505 <35416985+hoanglong0505@users.noreply.github.com>
2018-03-07 20:33:42 +0700

    [LongHH][Mearge]

--- Trunk/Source Code/Front-end/Hospital-portal/src/app/main/shiftScheduler/shiftScheduler-list.component.ts
+++ Trunk/Source Code/Front-end/Hospital-portal/src/app/main/shiftScheduler/shiftScheduler-list.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ShiftScheduler } from './shared/shiftScheduler.model';
 import { ShiftSchedulerService } from './service/shiftScheduler.service';
-<<<<<<< HEAD
 declare var $: any;
-=======
-
-declare var $:any;
->>>>>>> 06fcad47b0e6f3f66f996504852e51a4269d205b
 @Component({
     selector: 'shiftScheduler-list',
     templateUrl: './shiftScheduler-list.component.html'
@@ -28,27 +23,12 @@ export class ShiftSchedulerListComponent {
             alert(err);
             // this.loadingService.stop();
         });
-<<<<<<< HEAD
-
-=======
-        $( "#TEST" ).draggable({
-            
-        });
-        $( "#droppable" ).droppable({
-        drop: function( event, ui ) {
-            $(this)
-            .addClass( "ui-state-highlight" )
-            .find( "p" )
-                .html( "Dropped!" );
-        }
-        });
->>>>>>> 06fcad47b0e6f3f66f996504852e51a4269d205b
     }
 
 
     ngAfterViewInit() {
 
- 
+
 }
 
 
